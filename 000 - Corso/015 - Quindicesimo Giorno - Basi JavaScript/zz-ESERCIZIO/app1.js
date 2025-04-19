9fbff544ab1102e94bd09cb3e379229b5684f317
Dave02233 <davidecannistraro02@gmail.com>
2025-04-19 11:41:40 +0200

    JS

---
+++ 000 - Corso/015 - Quindicesimo Giorno - Basi JavaScript/zz-ESERCIZIO/app1.js
@@ -0,0 +1,29 @@
+//La mia età
+const myAge = 18;
+//Che cazzo scrivo?
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+//Esattamente quello che stai leggendo
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+
+//console.log(earlyYears, laterYears);
+
+//Calcolo anni del cane
+let myAgeInDogYears = earlyYears + laterYears;
+
+//Nome in LowerCase
+let myName = 'Davide'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
+
+
+
+
+
+
