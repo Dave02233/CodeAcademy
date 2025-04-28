88e70f54df535f6d45d39cb506147efbeca2fe91
Dave02233 <davidecannistraro02@gmail.com>
2025-04-28 13:56:00 +0200

    Objects

---
+++ 000 - Corso/019 - Diciannovesimo Giorno - Basi JS - Objects/AccessingObjects.js
@@ -0,0 +1,26 @@
+let spaceship = {
+    homePlanet: 'Earth',
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel',
+    numCrew: 5,
+    flightPath: ['Venus', 'Mars', 'Saturn']
+  };
+  
+  // Write your code below
+  const crewCount = spaceship.numCrew;
+  const planetArray = spaceship.flightPath;
+
+
+
+  let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    'Active Mission' : true,
+    homePlanet : 'Earth', 
+    numCrew: 5
+   };
+  
+  let propName =  'Active Mission';
+  
+  // Write your code below
+  const isActive = spaceship[propName];
+  console.log(isActive)
