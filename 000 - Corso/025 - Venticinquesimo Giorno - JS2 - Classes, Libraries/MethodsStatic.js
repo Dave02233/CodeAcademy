69646161cc1ae0e96c619ea83fc5c6c224c887ab
Dave02233 <davidecannistraro02@gmail.com>
2025-05-11 19:12:07 +0200

    Classi JS

---
+++ 000 - Corso/025 - Venticinquesimo Giorno - JS2 - Classes, Libraries/MethodsStatic.js
@@ -0,0 +1,69 @@
+/*
+class Animal {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+    
+  static generateName() {
+    const names = ['Angel', 'Spike', 'Buffy', 'Willow', 'Tara'];
+    const randomNumber = Math.floor(Math.random()*5);
+    return names[randomNumber];
+  }
+} 
+
+console.log(Animal.generateName()); // returns a name
+
+You cannot access the .generateName() method from instances of the Animal class or instances of its subclasses (See below).
+
+const tyson = new Animal('Tyson'); 
+tyson.generateName(); // TypeError
+
+
+The example above will result in an error, because you cannot call static methods (.generateName()) on an instance (tyson).
+*/
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+
+  static generatePassword() {
+    return Math.floor(Math.random() * 10000);
+  }
+}
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._certifications = certifications;
+  } 
+  
+  get certifications() {
+    return this._certifications;
+  }
+  
+  addCertification(newCertification) {
+    this.certifications.push(newCertification);
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
+
+console.log(HospitalEmployee.generatePassword())
