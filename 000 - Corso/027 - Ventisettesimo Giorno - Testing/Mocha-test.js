59182f3a308cde096572d61ac3a875cf1fce1b67
Dave02233 <davidecannistraro02@gmail.com>
2025-05-17 09:36:33 +0200

    Mocha I

---
+++ 000 - Corso/027 - Ventisettesimo Giorno - Testing/Mocha-test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('Math', () => {
+  describe('.min', () => {
+    it('returns the argument with the lowest value', () => {
+      assert.ok(3 + 4 === 8);
+    })
+  })
+})
