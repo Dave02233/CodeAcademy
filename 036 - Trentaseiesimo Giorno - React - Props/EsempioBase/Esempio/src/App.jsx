f6364cba36f0e4089dc92736e1ceb373ae3243e7
Dave02233 <davidecannistraro02@gmail.com>
2025-06-11 00:12:21 +0200

    props

--- 036 - Trentaseiesimo Giorno - React - Props/EsempioBase/Esempio/src/App.jsx
+++ 036 - Trentaseiesimo Giorno - React - Props/EsempioBase/Esempio/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-function App() {
+function App(props) {
   return(
     <>
       <h1>Hello World!</h1>
+      {props.text 
+      ? <button>{props.text}</button>
+      : <h2>Nesun testo prop</h2>}
     </>
   )
 }
