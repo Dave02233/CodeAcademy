3eb1d079bd328156538b49a9bed0f254b107b2c8
Dave <davidecannistraro02@gmail.com>
2026-01-15 21:42:28 +0100

    Fine corso python

---
+++ 084 - Ottantaquattresimo Giorno - Python - Advanced/attributes.py
@@ -0,0 +1,8 @@
+#Get all attributes
+
+print(dir(5))
+
+def this_function_is_an_object(num):
+  return "Cheese is {} times better than everything else".format(num)
+
+print(dir(this_function_is_an_object))
