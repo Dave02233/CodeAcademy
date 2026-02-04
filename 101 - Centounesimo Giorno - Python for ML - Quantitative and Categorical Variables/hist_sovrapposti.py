5474df51a1faada89f2d4950d87a17fa003f59d9
Dave <davidecannistraro02@gmail.com>
2026-02-04 22:55:56 +0100

    Quantitative and categorical + plots

---
+++ 101 - Centounesimo Giorno - Python for ML - Quantitative and Categorical Variables/hist_sovrapposti.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import codecademylib3
+import matplotlib.pyplot as plt
+
+students = pd.read_csv("students.csv")
+
+scores_urban = students.G3[students.address == "U"]
+scores_rural = students.G3[students.address == "R"]
+
+# create the overlapping histograms here:
+plt.hist(scores_urban, color="blue", label="U", normed=True, alpha=0.5)
+plt.hist(scores_rural, color="red", label="R", normed=True, alpha=0.5)
+
+plt.show()
