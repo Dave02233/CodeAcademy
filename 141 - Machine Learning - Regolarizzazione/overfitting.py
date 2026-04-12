b9718700d8e7b313401959f7b4cea8f419552785
Davide <davidecannistraro02@gmail.com>
2026-04-12 11:09:23 +0200

    overfitting

---
+++ 141 - Machine Learning - Regolarizzazione/overfitting.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import codecademylib3
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("./student_math.csv")
+print(df.head())
+
+#setting target and predictor variables
+y = df['Final_Grade']
+X = df.drop(columns = ['Final_Grade'])
+
+# 1. Number of features
+num_features = len(X.columns)
+print("Number of features: ",num_features)
+
+#Performing a Train-Test split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+#Fitting a Linear Regression Model
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+#Training Error
+pred_train = model.predict(X_train)
+MSE_train = np.mean((pred_train - y_train)**2)
+print("Training Error: ", MSE_train)
+
+# 2. Testing Error
+pred_test = model.predict(X_test)
+MSE_test = np.mean((pred_test - y_test)**2)
+print("Testing Error: ", MSE_test)
+
+#Calculating the regression coefficients
+predictors = X.columns
+coef = pd.Series(model.coef_,predictors).sort_values()
+
+# 3. Plotting the Coefficients
+
+plt.figure(figsize = (15,10))
+coef.plot(kind='bar', fontsize = 20)
+plt.title ("Regression Coefficients", fontsize = 30)
+plt.show()
+
+# Quando il modello ha troppi parametri o variabili molto simili tra loro (multi-collinearità),
+# può assegnare coefficienti con valori simili ma segno opposto a queste variabili.
+# Questo è un segnale di overfitting: il modello si adatta troppo ai dati di training,
+# anche al rumore, invece di trovare una soluzione più semplice e generalizzabile.
+
